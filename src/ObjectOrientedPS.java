a69602d83106540eac1f4f234665a86037412175
garimas14 <gs2432@srmist.edu.in>
2026-04-03 16:04:17 +0530

    garimas ADDED uc11

---
+++ src/ObjectOrientedPS.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class PalindromeChecker {
+    public boolean checkPalindrome(String input) {
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
+
+public class ObjectOrientedPS {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        PalindromeChecker checker = new PalindromeChecker();
+        boolean result = checker.checkPalindrome(input);
+
+        System.out.println("Is Palindrome? : " + result);
+        sc.close();
+    }
+}
